fd898dc540ddbaf2835c8285d36a2ae554608fb2
Aneesh Hunagund <h.aneesh@iitg.ac.in>
2026-06-04 21:50:55 +0530

    Update ALU_Unit.v

--- ALU_Unit.v
+++ ALU_Unit.v
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 08.04.2025 21:41:41
+// Create Date: 08.04.2026 21:41:41
 // Design Name: 
-// Module Name: ALU_unit
+// Module Name: arithmetic_logic_core
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -19,29 +19,28 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
-module ALU_unit(
-   input [3:0] alu_op,
-   input [31:0] operand_1,operand_2,
-   output [31:0] out,
-   output zero
-    );
+module arithmetic_logic_core(
+    input [3:0] ctrl_operation,
+    input [31:0] src_a, src_b,
+    output [31:0] result_val,
+    output is_zero_flag
+);
     
-   reg [31:0] out_d;  
+    reg [31:0] calc_result;  
 
-   
- always@(*)begin
- case(alu_op)
- 4'b0000: out_d=operand_1& operand_2;
- 4'b0001: out_d=operand_1|operand_2;
- 4'b0010: out_d=operand_1+ operand_2;
- 4'b0110: out_d=operand_1-operand_2;
- 4'b0111: out_d=operand_1>operand_2;
- 4'b1100: out_d=~(operand_1|operand_2);
- default:out_d=operand_1+ operand_2;
- endcase
- end
+    always @(*) begin
+        case(ctrl_operation)
+            4'b0000: calc_result = src_a & src_b;
+            4'b0001: calc_result = src_a | src_b;
+            4'b0010: calc_result = src_a + src_b;
+            4'b0110: calc_result = src_a - src_b;
+            4'b0111: calc_result = src_a > src_b;
+            4'b1100: calc_result = ~(src_a | src_b);
+            default: calc_result = src_a + src_b;
+        endcase
+    end
  
- assign out=out_d;
- assign zero=(out==32'b0);
+    assign result_val = calc_result;
+    assign is_zero_flag = (result_val == 32'b0);
+
 endmodule
